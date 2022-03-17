6e84cc75a4b02d594e2ceb59012897ca5bf0998b
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2022-03-17 14:44:17 +0200

    Update MovieDetails.test.js

--- src/components/MovieDetails/MovieDetails.test.js
+++ src/components/MovieDetails/MovieDetails.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "react-dom";
 import { MovieDetails } from "./MovieDetails";
 
-it('renders movie details poster', () => {
-    render(<MovieDetails />);
-    const PosterElement = screen.getBy()
-    expect(PosterElement).toBeInDocument()
+it("renders movie details poster", () => {
+  render(<MovieDetails />);
+  const PosterElement = screen.getByTestId();
+  expect(PosterElement).toBeInDocument();
 });
